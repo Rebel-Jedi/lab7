7f81229ad50fc479bf6f6ae9972de7047c21cf72
Rafael <rafaelavila1318@gmail.com>
2023-04-04 23:46:52 -0500

    func and var
    added a function and variable.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 function init(){
 	var button = document.getElementById('entrybutton');
+	function showMeText(){
+		var textbox = document.getElementById('entryinput');
+	}
 
 window.addEventListener('load', init);
